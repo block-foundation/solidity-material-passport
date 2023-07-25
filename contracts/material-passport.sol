47c4a3e4784ab12280528811329c1fe155c2e086
Lars van Vianen <lars@vianen.com>
2023-07-25 17:31:17 +0200

    Update material-passport.sol

--- contracts/material-passport.sol
+++ contracts/material-passport.sol
@@ -19,7 +19,38 @@
 pragma solidity ^0.8.19;
 
 
+// ============================================================================
+// Contracts
+// ============================================================================
+
+/**
+ * Material Passport Contract
+ * @dev 
+ */
 contract MaterialPassport {
+
+
+    // Parameters
+    // ========================================================================
+
+    // This creates an array with all materials
+    Material[] public materials;
+
+    // Structs
+    // ========================================================================
+
+    /**
+     * Material
+     * @dev 
+     * @param name The name
+     * @param origin The origin
+     * @param manufacturer The manufacturer
+     * @param description The description
+     * @param expirationDate The expirationDate
+     * @param isRecyclable The isRecyclable
+     * @param materialType The materialType
+     * @param lifecycle The lifecycle
+     */
     struct Material {
         string name;
         string origin;
@@ -31,8 +62,9 @@ contract MaterialPassport {
         string lifecycle;
     }
 
-    // This creates an array with all materials
-    Material[] public materials;
+
+    // Mappings
+    // ========================================================================
 
     // This creates a mapping from IDs to owners
     mapping(uint => address) public materialOwners;
@@ -40,39 +72,122 @@ contract MaterialPassport {
     // This creates a mapping from owners to their balances
     mapping(address => uint) public balances;
 
+
+    // Modifiers
+    // ========================================================================
+
     // This checks if the function caller is the owner of the material
     modifier onlyOwner(uint _materialId) {
-        require(materialOwners[_materialId] == msg.sender, "You are not the owner of this material");
+        require(
+            materialOwners[_materialId] == msg.sender,
+            "You are not the owner of this material"
+        );
         _;
     }
 
+
+    // Events
+    // ========================================================================
+
     // This creates an event that is triggered whenever a material is registered
-    event MaterialRegistered(uint materialId, address owner, string name, string origin, string manufacturer, string description, uint expirationDate, bool isRecyclable, string materialType, string lifecycle);
+    event MaterialRegistered(
+        uint materialId,
+        address owner,
+        string name,
+        string origin,
+        string manufacturer,
+        string description,
+        uint expirationDate,
+        bool isRecyclable,
+        string materialType,
+        string lifecycle
+    );
     
     // This creates an event that is triggered whenever a material is recycled
-    event MaterialRecycled(uint materialId, address recycler);
+    event MaterialRecycled(
+        uint materialId,
+        address recycler
+    );
     
     // This creates an event that is triggered whenever ownership of a material is transferred
-    event MaterialTransferred(uint materialId, address from, address to);
+    event MaterialTransferred(
+        uint materialId,
+        address from,
+        address to
+    );
+
+
+    // Methods
+    // ========================================================================
 
     // Registers a new material
-    function registerMaterial(string memory _name, string memory _origin, string memory _manufacturer, string memory _description, uint _expirationDate, bool _isRecyclable, string memory _materialType, string memory _lifecycle) public {
+    function registerMaterial(
+        string memory _name,
+        string memory _origin,
+        string memory _manufacturer,
+        string memory _description,
+        uint _expirationDate,
+        bool _isRecyclable,
+        string memory _materialType,
+        string memory _lifecycle
+    ) public {
         uint id = materials.length;
-        materials.push(Material(_name, _origin, _manufacturer, _description, _expirationDate, _isRecyclable, _materialType, _lifecycle));
+        materials.push(
+            Material(
+                _name,
+                _origin,
+                _manufacturer,
+                _description,
+                _expirationDate,
+                _isRecyclable,
+                _materialType,
+                _lifecycle
+            )
+        );
         materialOwners[id] = msg.sender;
-        emit MaterialRegistered(id, msg.sender, _name, _origin, _manufacturer, _description, _expirationDate, _isRecyclable, _materialType, _lifecycle);
+        emit MaterialRegistered(
+            id,
+            msg.sender,
+            _name,
+            _origin,
+            _manufacturer,
+            _description,
+            _expirationDate,
+            _isRecyclable,
+            _materialType,
+            _lifecycle
+        );
     }
 
     // Transfers ownership of a material
-    function transferMaterial(uint _materialId, address _newOwner) public onlyOwner(_materialId) {
+    function transferMaterial(
+        uint _materialId,
+        address _newOwner
+    ) public onlyOwner(
+        _materialId
+    ) {
         materialOwners[_materialId] = _newOwner;
-        emit MaterialTransferred(_materialId, msg.sender, _newOwner);
+        emit MaterialTransferred(
+            _materialId,
+            msg.sender,
+            _newOwner
+        );
     }
     
     // Recycles a material and rewards the recycler
-    function recycleMaterial(uint _materialId) public onlyOwner(_materialId) {
-        require(materials[_materialId].isRecyclable, "This material is not recyclable");
-        require(block.timestamp >= materials[_materialId].expirationDate, "This material has not yet expired");
+    function recycleMaterial(
+        uint _materialId
+    ) public onlyOwner(
+        _materialId
+    ) {
+        require(
+            materials[_materialId].isRecyclable,
+            "This material is not recyclable"
+        );
+        require(
+            block.timestamp >= materials[_materialId].expirationDate,
+            "This material has not yet expired"
+        );
         
         // We mark the material as recycled by setting the owner to the zero address
         materialOwners[_materialId] = address(0);
@@ -82,12 +197,28 @@ contract MaterialPassport {
     }
     
     // Update the lifecycle of a material
-    function updateLifecycle(uint _materialId, string memory _lifecycle) public onlyOwner(_materialId) {
+    function updateLifecycle(
+        uint _materialId,
+        string memory _lifecycle
+    ) public onlyOwner(
+        _materialId
+    ) {
         materials[_materialId].lifecycle = _lifecycle;
     }
 
     // Update the properties of a material
-    function updateMaterialProperties(uint _materialId, string memory _name, string memory _origin, string memory _manufacturer, string memory _description, uint _expirationDate, bool _isRecyclable, string memory _materialType) public onlyOwner(_materialId) {
+    function updateMaterialProperties(
+        uint _materialId,
+        string memory _name,
+        string memory _origin,
+        string memory _manufacturer,
+        string memory _description,
+        uint _expirationDate,
+        bool _isRecyclable,
+        string memory _materialType
+    ) public onlyOwner(
+        _materialId
+    ) {
         Material storage material = materials[_materialId];
         material.name = _name;
         material.origin = _origin;
